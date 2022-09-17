2c412070949fe05e52c850625a1aba887e4f79c9
darshgarala <110333279+darshgarala@users.noreply.github.com>
2022-09-17 20:30:08 +0530

    Create p4.java

---
+++ p4.java
@@ -0,0 +1,12 @@
+
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Lion extends Animal {
+    void eat() {
+        System.out.println("Lion is eating meat");
+    }
+}
